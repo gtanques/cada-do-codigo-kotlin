e2ed6d2708b0b56ed4d0b9c921271c1efe49f8e6
gtanques <saintcalm1@gmail.com>
2021-08-17 01:26:31 -0300

    findByEmail autor

--- br.com.orange/src/main/kotlin/casadocodigo/br/com/autores/BuscarDetalheAutorController.kt
+++ br.com.orange/src/main/kotlin/casadocodigo/br/com/autores/BuscarDetalheAutorController.kt
@@ -3,16 +3,32 @@ package casadocodigo.br.com.autores
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+import javax.transaction.Transactional
 
 @Controller("/autores")
 class BuscarDetalheAutorController(val autorRepository: AutorRepository) {
 
     @Get
-    fun listarAutores(): HttpResponse<List<DetalheAutorResponse>> {
-        val autores = autorRepository.findAll()
-        val resposta = autores.map { autor -> DetalheAutorResponse(autor) }
+    @Transactional
+    fun listarAutores(@QueryValue(defaultValue = "") email: String): HttpResponse<Any> {
+        if(email.isBlank()){
+            val autores = autorRepository.findAll()
+            val resposta = autores.map { autor -> DetalheAutorResponse(autor) }
 
-        return HttpResponse.ok(resposta)
+            return HttpResponse.ok(resposta)
+        }
+
+        val possivelAutor = autorRepository.findByEmail(email)
+
+        if (possivelAutor.isEmpty){
+            return HttpResponse.notFound()
+        }
+
+        val autor = possivelAutor.get()
+
+        return HttpResponse.ok(DetalheAutorResponse(autor))
     }
+
 
 }
